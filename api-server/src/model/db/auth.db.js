46ac420f08ba5aff2aa92cc7a70955d2765d3293
Braden Hitchcock <developer.hitchcock@gmail.com>
2019-01-17 20:03:37 -0800

    removing role from schema (not needed)

--- api-server/src/model/db/auth.db.js
+++ api-server/src/model/db/auth.db.js
@@ -1,13 +1,17 @@
 const mongoose = require('mongoose');
 
-const Auth = new mongoose.Schema({
-    _id: { type: String, required: true },
-    user: { type: String, required: true },
-    salt: { type: String, required: true },
-    hash: { type: String, required: true },
-    algo: { type: String, required: true },
-    role: { type: String, required: true },
-    last: { type: String, required: true }
-});
+const Auth = new mongoose.Schema(
+    {
+        _id: { type: String, required: true },
+        user: { type: String, required: true },
+        salt: { type: String, required: true },
+        hash: { type: String, required: true },
+        algo: { type: String, required: true },
+        last: { type: String, required: true }
+    },
+    {
+        strict: 'throw'
+    }
+);
 
 module.exports = mongoose.model('Auth', Auth);

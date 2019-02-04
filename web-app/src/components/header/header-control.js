26119e430aa2fa8f52bbab7533ed1ec5e78bdead
Braden Hitchcock <developer.hitchcock@gmail.com>
2019-02-04 09:25:09 -0800

    implementing logout

--- web-app/src/components/header/header-control.js
+++ web-app/src/components/header/header-control.js
@@ -6,16 +6,27 @@ class Header extends React.Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            userImage: null
+        };
+
         this.onSearch = this.onSearch.bind(this);
+        this.onLogout = this.onLogout.bind(this);
     }
 
     onSearch(query) {
         console.log(query);
     }
 
+    onLogout() {
+        // TODO: for a more secure logout, we need to also blacklist the JWT on the server side
+        document.cookie = 'auth=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+        this.props.history.push('/login');
+    }
+
     render() {
         const { match } = this.props;
-        return <HeaderView match={match} onSearch={this.onSearch} />;
+        return <HeaderView image={this.state.userImage} match={match} onSearch={this.onSearch} onLogout={this.onLogout} />;
     }
 }
 
